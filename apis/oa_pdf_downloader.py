7231b9f31dd6938c66507e38d597b48f5f91b332
Ardark27 <pacualadri@gmail.com>
2022-12-02 13:06:41 +0100

    0a download added

--- apis/oa_pdf_downloader.py
+++ apis/oa_pdf_downloader.py
@@ -1,12 +1,25 @@
 import pandas as pd
 import requests
 
-def get_pdf_url(data_path):
+def get_pdf_url_and_doi(data_path):
     data = pd.read_csv(data_path)
-    return data
+    return data["pdf_url"], data["dois_id"]
 
-def download_pdf(url):
+
+def download_pdf(url,name_of_pdf):
     r = requests.get(url)
-    with open('../pdf_info_extractor/data_pdf/' + url.split('/')[-1], 'wb') as f:
+    # replace dois_id / with _
+    name = name_of_pdf.replace('/','_')+ '.pdf'
+    with open('../pdf_info_extractor/data_pdf/' + name, 'wb') as f:
         f.write(r.content)
+
+def main():
+    oa_url,doi_id = get_pdf_url_and_doi('dois_not_arxiv_with_pdf_url.csv')
+    for url,doi in zip(oa_url,doi_id):
+        print('Downloading: ', url)
+        download_pdf(url,doi)
+        break #remove this line to download all the pdfs
+
+if __name__ == '__main__':
+    main()
 
