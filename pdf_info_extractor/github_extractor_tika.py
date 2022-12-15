921aa042267f45d684b34ec6710545305c456699
Ardark27 <pacualadri@gmail.com>
2022-12-15 12:15:37 +0100

    better treatment for no git_urls pdfs

--- pdf_info_extractor/github_extractor_tika.py
+++ pdf_info_extractor/github_extractor_tika.py
@@ -31,21 +31,28 @@ def save_github_urls(github_urls, output_path):
     return 200
 
 def filter_done(pdf_folder_path='data_pdf',
-                git_urls_folder_path='data_github_urls'):
+                git_urls_folder_path='data_github_urls',
+                no_urls_file='no_urls.txt'):
     # get the files in the folders
     pdf_files = os.listdir(pdf_folder_path)
     git_urls_files = os.listdir(git_urls_folder_path)
+    # read no_urls.txt
+    with open(no_urls_file, 'r') as f:
+        no_urls = f.read().split('\n')
     # remove the extension
     pdf_files = [x.replace('.pdf', '') for x in pdf_files]
     git_urls_files = [x.replace('.txt', '') for x in git_urls_files]
+    no_urls_files = [x.replace('.pdf', '') for x in no_urls]
     # convert to set
     pdf_files = set(pdf_files)
-    git_urls_files = set(git_urls_files)
+    git_urls_files = set(git_urls_files+no_urls_files)
     # get the difference
     not_done = pdf_files.difference(git_urls_files)
     # add the extension
     not_done = [x + '.pdf' for x in not_done]
+
     return not_done
+
 
 def pdf_to_git_url(folder_path = 'data_pdf', 
                       output_folder_path = 'data_github_urls'):
@@ -56,8 +63,13 @@ def pdf_to_git_url(folder_path = 'data_pdf',
         try:
             pdf_data = read_pdf(pdf_path)
             github_urls = look_for_github_urls(pdf_data)
-            output_path = os.path.join(output_folder_path, file_name.replace('.pdf', '.txt'))
-            save_github_urls(github_urls, output_path)
+            if github_urls:
+                output_path = os.path.join(output_folder_path, file_name.replace('.pdf', '.txt'))
+                save_github_urls(github_urls, output_path)
+            else:
+                print('no urls')
+                with open('no_urls.txt', 'a') as f:
+                    f.write(file_name + '\n')
         except:
             print('error')
             with open('error.txt', 'a') as f:
